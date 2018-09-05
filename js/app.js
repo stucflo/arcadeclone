28cd63545fa56d7eb988ae046391030de2d7e1f2
Stuart Campbell <stu@c-flo-enterprises.com>
2018-09-04 21:49:00 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,41 +1,94 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// Enemies the player must avoid
+class Enemy {
+    constructor(x, y) {
+        // Enemy Image
+        this.sprite = 'images/enemy-bug.png';
+        // Enemy Placement and Speed
+        this.x = x;
+        this.y = y;
+        this.speed = Math.floor((Math.random() * 200) + 200);
+    }
+    // Enemy Animation 
+    update(dt) {
+        // Keep image on screen 
+        if (this.x <= 550) {
+            this.x += this.speed * dt;
+        }
+        else {
+            this.x = -2;
+        }
+        //Enemy Collision Parameters
+        if (player.x >= this.x - 40 && player.x <= this.x + 60) {
+            if (player.y >= this.y - 30 && player.y <= this.y + 50) {
+                this.reset();
+            }
+        }
+    }
+}
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+// Player Starting Point
+class Player {
+    constructor() {
+        this.sprite = 'images/char-boy.png';
+        this.x = 200;
+        this.y = 400;
+    }
+    //Player Movement
+    update() {
+        // Move Player Left On Screen
+        if (this.ctlKey === 'left' && this.x > 0) {
+            this.x = this.x - 50;
+            //Move Player Right On Screen
+        }
+        else if (this.ctlKey === 'right' && this.x != 400) {
+            this.x = this.x + 50;
+            //Move Player Up On Screen
+        }
+        else if (this.ctlKey === 'up') {
+            this.y = this.y - 50;
+            //Move Player Down On Screen
+        }
+        else if (this.ctlKey === 'down' && this.y != 400) {
+            this.y = this.y + 50;
+        }
+        this.ctlKey = null;
+        //If on water, reset
+        if (this.y < 50) {
+            this.reset();
+            alert(" You Won! Do You Wish To Play Again?");
+        }
+    }
+    // Keys to control player 
+    handleInput(e) {
+        this.ctlKey = e;
+    }
+}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+const newLocal = Object.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+const newLocal_2 = Object.prototype.reset = function () {
+    player.x = 100;
+    player.y = 400;
+};
+
+// Load Enemie and Player Image
+const allEnemies = [];
+(function setEnemies(){
+    allEnemies.push(new Enemy(-2, 60));
+    allEnemies.push(new Enemy(-2, 100));
+    allEnemies.push(new Enemy(-2,150));
+    allEnemies.push(new Enemy(-2,220));
+}());
+
+const player = new Player(); 
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Keystroke event listener
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
